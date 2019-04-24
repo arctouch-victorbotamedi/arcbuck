8c7e016ebdb2a20299f0eecab2a3923fe24e5280
Victor Botamedi <victor.botamedi@arctouch.com>
2019-04-24 20:58:35 -0300

    :lipstick: Add extended app bar

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -16,18 +16,27 @@ class _HomeViewState extends State<HomeView> {
   int _count = 0;
 
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var mainColor = theme.primaryColor;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
+        centerTitle: true,
         title: Text('April Budget'),
       ),
-      body: _buildCenterWidget(context),
+      body: _buildCenterWidget(context, theme),
       bottomNavigationBar: BottomAppBar(
         child: Container(
           height: 50.0,
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        shape: StadiumBorder(
+            side: BorderSide(
+                color: mainColor.withAlpha(25),
+                width: 50,
+            )
+        ),
         onPressed: () => setState(() {
           _count++;
         }),
@@ -38,34 +47,53 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 
-  Widget _buildCenterWidget(BuildContext context) {
-    var bloc = BlocProvider.of<EventBloc>(context);
-    return BlocBuilder(
-      bloc: bloc,
-      builder: (context, state) {
-        switch (state.runtimeType) {
-          //case UninitializedState:
-          //  return _buildUninitilazedList();
-          case EventsLoadedState:
-            return _buildEventListWidget(bloc, state);
-          default:
-            return Center(child: Text('Failed to fetch'));
-        }
-      }
+  Widget _buildCenterWidget(BuildContext context, ThemeData theme) {
+    return Column(
+      children: [
+        _buildHeaderWidget(context, theme),
+        _buildEventsContainerWidget(context, theme)
+      ],
     );
   }
 
-  Widget _buildEventListWidget(EventBloc bloc, EventsLoadedState state) {
-    return ListView.builder(
-      itemCount: state.events.length,
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, index) {
-        //var currentPercentage =  (index * 100) / state.movies.length;
-        //if (currentPercentage >= _scrollThresholdPercentage) {
-        //  bloc.dispatch(Fetch());
-        //}
-        return EventListItem(state.events[index]);
-      },
+  Widget _buildHeaderWidget(BuildContext context, ThemeData theme) =>
+    Container(
+        width: double.infinity,
+        color: theme.primaryColor,
+        height: 81,
+    );
+
+
+  Widget _buildEventsContainerWidget(BuildContext context, ThemeData theme) {
+    var bloc = BlocProvider.of<EventBloc>(context);
+    return BlocBuilder(
+        bloc: bloc,
+        builder: (context, state) {
+          switch (state.runtimeType) {
+          //case UninitializedState:
+          //  return _buildUninitilazedList();
+            case EventsLoadedState:
+              return _buildEventListWidget(bloc, state);
+            default:
+              return Center(child: Text('Failed to fetch'));
+          }
+        }
     );
   }
+
+  Widget _buildEventListWidget(EventBloc bloc, EventsLoadedState state) =>
+      Flexible(
+          child: ListView.builder(
+            itemCount: state.events.length,
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: (context, index) {
+              //var currentPercentage =  (index * 100) / state.movies.length;
+              //if (currentPercentage >= _scrollThresholdPercentage) {
+              //  bloc.dispatch(Fetch());
+              //}
+              return EventListItem(state.events[index]);
+            },
+          )
+      );
+
 }
