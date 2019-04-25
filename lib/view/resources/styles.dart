888e9fd589e1f74c41ba9ab64d2408941d49a421
Sidnei Polo <sidnei.polo@arctouch.com>
2019-04-25 18:24:12 -0300

    :art: Styles file added

---
+++ lib/view/resources/styles.dart
@@ -0,0 +1,12 @@
+import 'package:arcbuck/view/resources/theme_colors.dart';
+import 'package:flutter/cupertino.dart';
+
+abstract class Styles {
+
+  static const shadowStyle = BoxShadow(
+    spreadRadius: 0,
+    color: ThemeColors.shadowColor,
+    blurRadius: 15.0,
+  );
+
+}
